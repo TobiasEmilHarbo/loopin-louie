dcbebd3c57b5dfa5186ae1b0fbbe5cced7d3bbd6
TobiasEmilHarbo <tobiasharbo@gmail.com>
2016-03-29 16:32:55 +0200

    plane

---
+++ Assets/plane.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+private var head : Transform;
+
+function Start ()
+{
+	head = GameObject.Find("Louie").transform;
+}
+
+function Update ()
+{
+	transform.eulerAngles.y = head.eulerAngles.y;
+	print(head.eulerAngles.y);
+}
