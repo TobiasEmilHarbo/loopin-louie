1732b254ebadf601a99fb16be18dc1823705d35b
TobiasEmilHarbo <tobiasharbo@gmail.com>
2016-04-01 13:32:18 +0200

    anim

---
+++ Assets/trigger.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	
+	if (collision.gameObject.tag == "Plane")
+	{
+		collision.animation.Play ("airplane"); 
+		// we want the player's animation to play, not the trigger's.
+	}
+}
