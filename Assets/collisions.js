4076b4faa4ea7a25c05d7bf1f141f27334ccf8be
TobiasEmilHarbo <tobiasharbo@gmail.com>
2016-04-04 16:44:04 +0200

    flutflut

--- Assets/collisions.js
+++ Assets/collisions.js
@@ -9,6 +9,8 @@ var henFlutter : Transform;
 private var Henp1 : GameObject;
 private var Henp3 : GameObject;
 
+var henClones = new Array();
+
 function Start ()
 {
 	Henp1 = GameObject.Find("Henp1");
@@ -17,7 +19,10 @@ function Start ()
 
 function Update ()
 {
-	
+	for (var henClone : Transform in henClones)
+	{
+		henClone.Translate(Vector3.up * Time.deltaTime * 3, Space.World);
+	}
 }
 
 function OnTriggerEnter(col : Collider)
@@ -28,20 +33,19 @@ function OnTriggerEnter(col : Collider)
 	}
 	else if(col.gameObject.name == "Henp1")
 	{
-
 		henDownP1++;
 		var henCloneP1 : Transform = Instantiate(henFlutter, transform.position, Quaternion.identity);
+		henClones.Push(henCloneP1);
 		henCloneP1.transform.parent = Henp1.transform;
 
 		//henCloneP1.GetComponent.<Animation>().Play("henAway");
-
 	}
 	else if(col.gameObject.name == "Henp3")
 	{
 		henDownP3++;
 		var henCloneP3 : Transform = Instantiate(henFlutter, transform.position, Quaternion.identity);
+		henClones.Push(henCloneP3);
 		henCloneP3.transform.parent = Henp3.transform;
-
 	}
 
 	// if()
